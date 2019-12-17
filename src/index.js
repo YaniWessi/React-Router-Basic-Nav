4cd10d8c30b0c1b44b2e3a552ebc04e0e954623b
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2019-12-17 16:16:12 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
 
-<Router>   
- <App />
-</Router>, 
-
-rootElement);
+  rootElement
+);
